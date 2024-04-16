6cb3dc68eaf0f4adcb257361bb941064f9e65532
lushuiyu <lu.zeqi@qq.com>
2024-04-16 19:04:18 +0800

    feat: tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.{html,js,liquid}",
+    "./layout/**/*.{html,js,liquid}",
+    "./sections/**/*.{html,js,liquid}",
+    "./snippets/**/*.{html,js,liquid}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
